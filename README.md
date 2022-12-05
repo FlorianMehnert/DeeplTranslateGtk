9e4a2ceb0ff06e11cda5e24eceb575d07705444d
Florian Mehnert <florian.mehnert@gmail.com>
2022-12-05 15:38:51 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Translator using Deepl Api
+
+- to use this tranlator you need a Deepl account with at least the free plan (you will be able to translate up to
+  500.000 characters per month)
+- using this also requires GTK 3.0 ... make sure to set it up :)
+- tested on Ubuntu 22.04
+
+## Usage
+
+If you want to be convenient just set up your own hotkey for the deepl_translation.py in your custom
+keybindings: `python3 PATH_TO_deepl_translation.py` (using the gnome GUI)
+
+1. (optional: copy something to your clipboard)
+2. start the script
+3. (optional: replace the text in the Entry)
+4. <kbd>ENTER</kbd> to start translating
+5. <kbd>ENTER</kbd> again to close the Entry which should now contain the translated text
---
+++ deepl_translation.py
@@ -0,0 +1,73 @@
+import json
+import os
+import requests
+import gi
+
+gi.require_version("Gtk", "3.0")
+gi.require_version('Gdk', '3.0')
+from gi.overrides import Gdk
+
+from gi.repository import Gtk
+
+
+# class to show textinput
+class EntryWindow(Gtk.Window):
+    def __init__(self, content):
+        super().__init__(title="translate to DE")
+        self.set_size_request(400, 70)
+        self.connect("destroy", Gtk.main_quit)
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(vbox)
+
+        self.entry = Gtk.Entry()
+        self.entry.set_text(content)
+        self.entry.connect("activate", self.on_entry_activate)
+        vbox.pack_start(self.entry, True, True, 0)
+
+        self.state = 0
+
+    def on_entry_activate(self, widget):
+        if self.state:
+            self.destroy()
+        else:
+            data = f"text={self.entry.get_text()}&target_lang={target}"
+            response = requests.post('https://api-free.deepl.com/v2/translate', headers=headers, data=data)
+            b = json.loads(response.text)
+            print(b)
+            language = "unknown"
+            text = ""
+            try:
+                language = b["translations"][0].get("detected_source_language")
+            except Exception as e:
+                print(e)
+            try:
+                text = b["translations"][0].get("text")
+            except Exception as e:
+                print(e)
+
+            print(language)
+            self.entry.set_text(text)
+            self.state += 1
+
+
+if __name__ == "__main__":
+    target = "DE"
+    print(os.listdir())
+    with open("DeeplTranslateGtk/auth.txt", "r") as f:
+        auth = f.readline()
+
+    headers = {
+        'Authorization': f'DeepL-Auth-Key {auth}',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+    # 1.clipboard
+    clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+    text = clipboard.wait_for_text()
+
+    # 2. aufrufen des Programms mit der Zwischenablage
+    # 3a + 3b
+    ew = EntryWindow(text)
+    ew.show_all()
+
+    Gtk.main()
